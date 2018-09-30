0be3b57a9a03c667a9978dc55d10874f495bddf2
SAM2O2O <an.guoyue254@gmail.com>
2018-09-30 11:55:15 +0800

    add gotoUrl

--- src/lib/Util/ZalyText.php
+++ src/lib/Util/ZalyText.php
@@ -15,7 +15,7 @@ class ZalyText
 
     public static $texts = [
         "text.group.notExists" => ["group is not exists", "当前群组不存在"],
-        "text.group.notSpeaker" => ["only speakers and admin can speak", "当前只允许群管理以及发言者发言"],
+        "text.group.notSpeaker" => ["only speakers and admin can speak,speakers are ", "当前只允许群管理以及发言者发言，发言人者："],
         "text.group.notMember" => ["you aren't group member", "你不是群组成员"],
         "text.group.create" => ["group created,invite your friends to join chat", "群组已创建成功,邀请你的好友加入群聊吧"],
     ];
