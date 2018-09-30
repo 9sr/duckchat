3667e636f6b0e3c2d251fc4b28b786d1469fc085
childeYin <zhangjun516@126.com>
2018-09-30 18:38:58 +0800

    del useless code

--- src/controller/InstallDBController.php
+++ src/controller/InstallDBController.php
@@ -324,7 +324,6 @@ class InstallDBController
         $ownerUic = $this->uic;
         $this->_insertSiteOwnerUic($ownerUic);
         $this->initPluginMiniProgram();
-        $this->_initSiteUserGif();
         return;
     }
 
@@ -496,33 +495,6 @@ class InstallDBController
             }
         }
         $this->logger->info("site.install.db", "init miniPrograms finish success=" . json_encode($successParams));
-    }
-
-    private function _initSiteUserGif()
-    {
-        $tag = __CLASS__ . "->" . __FUNCTION__;
-
-            for($i=1; $i<8; $i++) {
-                try{
-                    $dirName = WPF_LIB_DIR . "/../{$this->attachmentDir}/default";
-                    if (!is_dir($dirName)) {
-                        mkdir($dirName, 0755, true);
-                    }
-                    $gifDirName =   WPF_LIB_DIR . "/../{$this->gifDir}/default";
-                    $this->db->beginTransaction();
-                    $gifId = ZalyHelper::generateStrKey();
-                    $dataSql = "insert into siteGif (gifId, gifUrl, width, height) VALUES ('{$gifId}', 'default-{$i}.gif', 200, 200);";
-                    $this->db->exec($dataSql);
-                    $dataSql = "insert into siteUserGif(userId, gifId) VALUES ('duckchat', '{$gifId}');";
-                    $this->db->exec($dataSql);
-                    $this->db->commit();
-                    rename($gifDirName."/".$i.".gif", $dirName."/".$i.".gif");
-                }catch (Exception $ex) {
-                    $this->db->rollBack();
-                    $this->logger->error($tag, $ex);
-                }
-            }
-        $this->logger->info("site.install.db", "init siteUserGif finish success=");
     }
 
     public function insertData($tableName, $data)
